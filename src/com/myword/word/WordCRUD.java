05be12d3453dcdc22ed668756cfe9a40be446fef
origin <https://github.com/@zzozzo0427/PP1_Project1.git>
2023-09-16 12:50:21 +0900

    수정

--- src/com/myword/word/WordCRUD.java
+++ src/com/myword/word/WordCRUD.java
@@ -20,11 +20,11 @@ public class WordCRUD implements ICRUD{
 
     @Override
     public Object add() {
-        System.out.println("=> 난이도(1, 2, 3) & 새 단어 입력 : ");
+        System.out.print("=> 난이도(1, 2, 3) & 새 단어 입력 : ");
         int level = s.nextInt();
         String word = s.nextLine();
 
-        System.out.println("뜻 입력 : ");
+        System.out.print("뜻 입력 : ");
         String meaning = s.nextLine();
 
         return new Word(0, level, word, meaning);
